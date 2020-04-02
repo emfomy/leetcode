ba1e4a060526bb4a18a83afa3f344859ad472745
Mu Yang <emfomy@gmail.com>
2020-04-03 00:49:49 +0800

    Update readme.

--- README.md
+++ README.md
@@ -5,14 +5,10 @@ LeetCode Problems' Solutions
 
 | # | Title | Solution | Difficulty |
 |---| ----- | -------- | ---------- |
-| 1 | [Two Sum](//leetcode.com/problems/two-sum/) | [C++](./problems/algorithms/0001.cpp) | Easy |
-| 1 | [Two Sum](//leetcode.com/problems/two-sum/) | [Python](./problems/algorithms/0001.py) | Easy |
-| 2 | [Add Two Numbers](//leetcode.com/problems/add-two-numbers/) | [C++](./problems/algorithms/0002.cpp) | Medium |
-| 2 | [Add Two Numbers](//leetcode.com/problems/add-two-numbers/) | [Python](./problems/algorithms/0002.py) | Medium |
-| 3 | [Longest Substring Without Repeating Characters](//leetcode.com/problems/longest-substring-without-repeating-characters/) | [C++](./problems/algorithms/0003.cpp) | Medium |
-| 3 | [Longest Substring Without Repeating Characters](//leetcode.com/problems/longest-substring-without-repeating-characters/) | [Python](./problems/algorithms/0003.py) | Medium |
-| 4 | [Median of Two Sorted Arrays](//leetcode.com/problems/median-of-two-sorted-arrays/) | [C++](./problems/algorithms/0004.cpp) | Hard |
-| 4 | [Median of Two Sorted Arrays](//leetcode.com/problems/median-of-two-sorted-arrays/) | [Python](./problems/algorithms/0004.py) | Hard |
+| 1 | [Two Sum](//leetcode.com/problems/two-sum/) | [C++](./problems/algorithms/0001.cpp) [Python](./problems/algorithms/0001.py) | Easy |
+| 2 | [Add Two Numbers](//leetcode.com/problems/add-two-numbers/) | [C++](./problems/algorithms/0002.cpp) [Python](./problems/algorithms/0002.py) | Medium |
+| 3 | [Longest Substring Without Repeating Characters](//leetcode.com/problems/longest-substring-without-repeating-characters/) | [C++](./problems/algorithms/0003.cpp) [Python](./problems/algorithms/0003.py) | Medium |
+| 4 | [Median of Two Sorted Arrays](//leetcode.com/problems/median-of-two-sorted-arrays/) | [C++](./problems/algorithms/0004.cpp) [Python](./problems/algorithms/0004.py) | Hard |
 
 # Concurrency
 
--- scripts/readme.py
+++ scripts/readme.py
@@ -15,6 +15,37 @@ LANG = {
 
 def main():
 
+    data = {}
+    # Categories
+    for cat_dir in glob.glob('./problems/*'):
+        cat = os.path.basename(cat_dir).capitalize()
+        data[cat] = {}
+
+        for prob_file in sorted(glob.glob(f'{cat_dir}/*')):
+            prob_num, prob_lang = os.path.splitext(os.path.basename(prob_file))
+            prob_num = int(prob_num)
+            prob_lang = LANG[prob_lang]
+            with open(prob_file) as fin:
+                prob_source = fin.readline().split(':')[-1].strip()
+                prob_title = fin.readline().split(':')[-1].strip()
+                prob_difficulty = fin.readline().split(':')[-1].strip()
+
+            if prob_num not in data[cat]:
+                data[cat][prob_num] = {
+                    'source': prob_source,
+                    'title': prob_title,
+                    'difficulty': prob_difficulty,
+                    'solution': {
+                        prob_lang: prob_file,
+                    }
+                }
+            else:
+                prob = data[cat][prob_num]
+                assert prob['source'] == prob_source
+                assert prob['title'] == prob_title
+                assert prob['difficulty'] == prob_difficulty
+                prob['solution'][prob_lang] = prob_file
+
     with open('./README.md', 'w') as fout:
 
         # Header
@@ -23,24 +54,20 @@ def main():
         print(file=fout)
 
         # Categories
-        for cat_dir in sorted(glob.glob('./problems/*')):
-            cat = os.path.basename(cat_dir).capitalize()
+        for cat in sorted(data.keys()):
             print(f'# {cat}', file=fout)
             print(file=fout)
 
             # Table
             print('| # | Title | Solution | Difficulty |', file=fout)
             print('|---| ----- | -------- | ---------- |', file=fout)
-            for prob_file in sorted(glob.glob(f'{cat_dir}/*')):
-                prob_num, prob_lang = os.path.splitext(os.path.basename(prob_file))
-                prob_num = int(prob_num)
-                prob_lang = LANG[prob_lang]
-                with open(prob_file) as fin:
-                    prob_source = fin.readline().split(':')[-1].strip()
-                    prob_title = fin.readline().split(':')[-1].strip()
-                    prob_diff = fin.readline().split(':')[-1].strip()
-
-                print(f'| {prob_num} | [{prob_title}]({prob_source}) | [{prob_lang}]({prob_file}) | {prob_diff} |', file=fout)
+            for prob_num in sorted(data[cat].keys()):
+                prob = data[cat][prob_num]
+                prob_title = prob['title']
+                prob_source = prob['source']
+                prob_difficulty = prob['difficulty']
+                solutions = ' '.join((f'[{lang}]({file})' for lang, file in sorted(prob['solution'].items())))
+                print(f'| {prob_num} | [{prob_title}]({prob_source}) | {solutions} | {prob_difficulty} |', file=fout)
             print(file=fout)
 
 if __name__ == '__main__':
